ebbbe85ab728eef1291439aed6dad1b606d1e8e0
Julius Garrido <jflgarrido@gmail.com>
2022-03-23 10:35:59 -0600

    commit tree d1d847eb871d8817ef24f847cfd47f9aa1815b12

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn(any)
+        @knowledge << any
+    end
+    def knowledge
+        @knowledge
+    end
 
 end
